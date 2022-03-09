85b756d1cf9b621047e45dc1e8155763da4eba5f
Quicksilver32 <ameliya9@mail.ru>
2022-03-09 11:51:41 +0300

    Revert "task-cycles"
    This reverts commit 566c88b38bd4c3381db048a70688da3a2799c093.

--- src/Cycles.js
+++ src/Cycles.js
@@ -3,45 +3,18 @@
   Используя цикл for, просуммируйте все четные числа в диапазоне между этими значениями (включительно)
   и верните итоговый результат.
 */
-export function rangeSum(start, end) {
-    let result = 0;
-    for (let i = start; i <= end; i++) {
-        if (i % 2 === 0) {
-            result += i;
-        }
-    }
-    return result;
-}
+export function rangeSum(start, end) {}
 
 /*
   В функцию iterationCount() приходит неотрицательное число.
   Используя цикл while, выполняйте деление этого числа на два до тех пор, пока результат деления больше 0.1
   и верните количество потребовавшихся итераций (т.е. сколько раз пришлось выполнить деление).
 */
-export function iterationCount(a) {
-    let count = 0;
-    while (a > 0.1) {
-        count += 1;
-        a /= 2;
-    }
-    return count;
-}
+export function iterationCount(a) {}
 
 /*
   В функцию symbolsReplace() приходит строка текста.
   Используя цикл do while, замените в тексте каждый третий символ на символ нижнего подчеркивания
   и верните итоговый результат.
 */
-export function symbolsReplace(message) {
-    let i = 1;
-    let result = '';
-    do {
-        if (i % 3 === 0) {
-            result += '_';
-        } else {
-            result += message[i - 1];
-        }
-        i++;
-    } while (i <= message.length);
-    return result;
-}
+export function symbolsReplace(message) {}
